7a1a9ff5b84097169015409a28eff9abbe62bf11
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-06-09 18:39:32 +0200

    Admin Dashboard

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,8 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            'is_admin' => $request->user()->hasRole('admin')
+            //'is_admin' => $request->user() ? $request->user()->hasRole('admin') : null
+            'is_admin' => $request->user() ? $request->user()->hasRole('admin') : false
         ]);
     }
 }
