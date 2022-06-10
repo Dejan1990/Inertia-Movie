cba76955a6e85052a0e16c6e06d4e7672c0ab294
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-06-10 14:41:26 +0200

    Admin route

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -55,8 +55,8 @@ const logout = () => {
 
                                 <JetNavLink 
                                     v-if="$page.props.is_admin" 
-                                    :href="route('dashboard')" 
-                                    :active="route().current('dashboard')"
+                                    :href="route('admin.index')" 
+                                    :active="route().current('admin.index')"
                                 >
                                     Admin
                                 </JetNavLink>
